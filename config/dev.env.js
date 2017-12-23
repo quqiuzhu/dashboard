6400019891836253a22ea451f741f6bac4f0090a
屈秋竹 <me@quqiuzhu.com>
2017-12-24 03:36:21 +0800

    转向https

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://ticks.quqiuzhu.com/api"',
+  BASE_API: '"https://ticks.quqiuzhu.com/api"',
 })
