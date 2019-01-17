ca8c959effc11768380b2105bc6c7fd1b023ae75
屈秋竹 <me@quqiuzhu.com>
2019-01-17 22:10:53 +0800

    fix OOPS page

--- src/views/common/404.vue
+++ src/views/common/404.vue
@@ -2,10 +2,10 @@
   <div class="background">
     <div class="wscn-http404">
       <div class="pic-404">
-        <img class="pic-404__parent" :src="img_404" alt="404">
-        <img class="pic-404__child left" :src="img_404_cloud" alt="404">
-        <img class="pic-404__child mid" :src="img_404_cloud" alt="404">
-        <img class="pic-404__child right" :src="img_404_cloud" alt="404">
+        <img class="pic-404__parent" src="/static/404_images/404.png" alt="404">
+        <img class="pic-404__child left" src="/static/404_images/404_cloud.png" alt="404">
+        <img class="pic-404__child mid" src="/static/404_images/404_cloud.png" alt="404">
+        <img class="pic-404__child right" src="/static/404_images/404_cloud.png" alt="404">
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
@@ -21,13 +21,7 @@
  export default {
    data() {
      return {
-       img_404: '/static/404_images/404.png',
-       img_404_cloud: '/static/404_images/404_cloud.png'
-     }
-   },
-   computed: {
-     message() {
-       return '特朗普说这个页面你不能进......'
+       message: '特朗普说这个页面你不能进......'
      }
    }
  }
